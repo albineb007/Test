8c6e7cd04fdedbee1bf628a270d38302c1376eab
Albin EB <74339427+albineb007@users.noreply.github.com>
2025-08-06 05:14:20 +0000

    main

--- core/urls.py
+++ core/urls.py
@@ -5,11 +5,11 @@ from .views import hello_world
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', hello_world),
+    path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('how-it-works/', views.how_it_works, name='how_it_works'),
     path('privacy/', views.privacy_policy, name='privacy_policy'),
     path('terms/', views.terms_of_service, name='terms_of_service'),
-    path('', hello_world),
 ]
